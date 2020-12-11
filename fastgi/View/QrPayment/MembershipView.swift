5f48e6358c5e90f976fb178e196e20301aa08f2f
omar <you@gmail.com>
2020-12-11 19:03:03 -0400

    fix_membershipView

--- fastgi/View/QrPayment/MembershipView.swift
+++ fastgi/View/QrPayment/MembershipView.swift
@@ -10,45 +10,37 @@ import SwiftUI
 struct MembershipView: View {
     @State private var action:Int? = 0
     var body: some View {
-       // Text("asd")
-        HStack{
-            Button(action: {
-                self.action = 1
-            }){
+        ScrollView{
+            VStack{
+                Text("Seleccione servicio")
+                    .font(.caption)
+                    //.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
+                    .padding(.vertical,10)
                 HStack{
-                    Image("Transport")
-                        .resizable()
-                        .frame(width:80, height: 80)
-                        .padding(10)
+                    Button(action: {
+                        self.action = 1
+                    }){
+                        HStack(spacing:10){
+                            Image("Transport")
+                                .resizable()
+                                .frame(width:80, height: 80)
+                                .padding(10)
+                                //Spacer()
+                                //.frame(maxWidth:.infinity)
+                        }
+                        .background(Color("card"))
+                        .cornerRadius(10)
+                        //.frame(maxWidth:.infinity)
+                        .shadow(color: Color.black.opacity(0.1), radius: 4, x: 2, y: 3)
+                        
+                    }
+                    NavigationLink(destination: SlideFormJoinView(), tag: 1, selection: self.$action) {
+                        EmptyView()
+                    }
+                  
                 }
-                .background(Color("card"))
-                .cornerRadius(10)
-                .frame(maxWidth:.infinity)
-                .shadow(color: Color.black.opacity(0.1), radius: 4, x: 2, y: 3)
-                
             }
-            NavigationLink(destination: SlideFormJoinView(), tag: 1, selection: self.$action) {
-                EmptyView()
-            }
-           /* Button(action: {
-                self.action = 2
-            }){
-                HStack{
-                    Image("Transport")
-                        .resizable()
-                        .frame(width:80, height: 80)
-                        .padding(10)
-                }
-                .background(Color("card"))
-                .cornerRadius(10)
-                .frame(maxWidth:.infinity)
-                .shadow(color: Color.black.opacity(0.1), radius: 4, x: 2, y: 3)
-                
-            }
-            NavigationLink(destination: RegistrationFormView1(), tag: 2, selection: self.$action) {
-                EmptyView()
-            }*/
-        }
+        }.padding()
      
     }
 }
@@ -58,3 +50,22 @@ struct MembershipView_Previews: PreviewProvider {
         MembershipView()
     }
 }
+
+/* Button(action: {
+     self.action = 2
+ }){
+     HStack{
+         Image("Transport")
+             .resizable()
+             .frame(width:80, height: 80)
+             .padding(10)
+     }
+     .background(Color("card"))
+     .cornerRadius(10)
+     .frame(maxWidth:.infinity)
+     .shadow(color: Color.black.opacity(0.1), radius: 4, x: 2, y: 3)
+     
+ }
+ NavigationLink(destination: RegistrationFormView1(), tag: 2, selection: self.$action) {
+     EmptyView()
+ }*/
